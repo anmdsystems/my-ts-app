4e081f79ca0b69840b4a83b9b6c3021e66625d06
Milind Date <msdate@gmail.com>
2022-07-16 22:21:01 -0400

    Sample test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'sampletest';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import {ApricusTableProps as ApricusTableProps, 
+//   ApricusTable, ApricusTableSelectedAction, ApricusTableAction, Counter} from 'apricus-react-lib';
+
+function App() { 
+  return <Button label="Test" ></Button>
 }
 
 export default App;
